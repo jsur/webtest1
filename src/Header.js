a5cf03a54d5dd7e1a4f722810592dea80ce5fcbd
Julius Suominen <julius.suominen@gmail.com>
2018-07-15 22:23:45 +0200

    1st commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="main-header">
+        <h1>Test</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      cards: [
+        {
+          id: 1,
+          topic: 'Design',
+          iconUrl: 'https://image.flaticon.com/icons/svg/997/997252.svg',
+          description: 'description description'
+        },
+        {
+          id: 2,
+          topic: 'Web applications',
+          iconUrl: 'https://image.flaticon.com/icons/svg/997/997225.svg',
+          description: 'description description'
+        },
+        {
+          id: 3,
+          topic: 'Maintenance',
+          iconUrl: 'https://image.flaticon.com/icons/svg/1018/1018694.svg',
+          description: 'description description'
+        }
+      ]
+    }
+  }
+
+  render() {
+
+    const cards = this.state.cards.map(item => {
+      return (
+        <div className="card" key={item.id}>
+          <div className="card-img">
+            <img src={item.iconUrl} alt="card-img"/>
+          </div>
+          <div className="card-content">
+            <p>{item.topic}</p>
+          </div>
+        </div>
+      )
+    });
+
+
+    return (
+      <main className="main">
+        <div className="main-img-layer">
+
+        </div>
+        <section className="main-section-1">
+          <div className="main-section-1-content">
+            <h1>We are nnn Consulting</h1>
+            <div>
+              <p>We create websites</p>
+            </div>
+          </div>
+        </section>
+        <section className="main-section-2">
+          <div className="card-container">
+          {cards}
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Main;
